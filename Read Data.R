386c860d00a9746e90dd8a7801e754c01c01b4ef
Ellie Christman <epc323@lehigh.edu>
2021-02-18 12:46:06 -0500

    Added file to read data

---
+++ Read Data.R
@@ -0,0 +1,9 @@
+# Store current working directory
+projdir <- getwd()
+projdir
+
+# Change working directory to covid-19-data folder
+setwd("./covid-19-data/")
+US <- read.csv("us.csv") 
+setwd(projdir)
+
